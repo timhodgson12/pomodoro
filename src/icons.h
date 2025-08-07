7615e27bc4f6b0840cad0f4ec68f559f30c86561
Tim Hodgson <timhodgson@protonmail.com>
2025-08-07 17:08:34 -0400

    Reorder image includes in images.h for consistency and clarity

--- src/icons.h
+++ src/icons.h
@@ -3,16 +3,15 @@
 
 #include "icon.h"
 
+extern Icon icon_lpethink;
 extern Icon icon_email;
-extern Icon icon_warning;
-extern Icon icon_focus;
-extern Icon icon_coffee;
-extern Icon icon_lpetantrum;
-extern Icon icon_lpehacker;
 extern Icon icon_checkmark;
 extern Icon icon_lpenote;
-extern Icon icon_lpethink;
-extern Icon icon_lpesip;
+extern Icon icon_lpetantrum;
+extern Icon icon_lpehacker;
 extern Icon icon_coding;
+extern Icon icon_lpesip;
+extern Icon icon_focus;
+extern Icon icon_coffee;
 
 #endif // ICONS_H
